d43634244e3d7a1bbc7f5363cdf03704bddc020f
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-06-27 18:26:46 +0000

    generator card finish, solo falta modificadores de anchura y altura

--- src/app.js
+++ src/app.js
@@ -2,51 +2,62 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
+function generadedPalos() {
+  const suits = ["♦", "♥", "♠", "♣"];
 
-  function generadedPalos() {
-    const suits = ["♦", "♥", "♠", "♣"];
+  let suitRandom = Math.floor(Math.random() * suits.length);
+  return suits[suitRandom];
+}
 
-    let suitRandom = Math.floor(Math.random() * suits.length);
-    return suits[suitRandom];
-  }
-  function generadedNumber() {
-    const cardValue = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-      "A"
-    ];
-    let numberRandom = Math.floor(Math.random() * cardValue.length);
+function generadedNumber() {
+  const cardValue = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let numberRandom = Math.floor(Math.random() * cardValue.length);
 
-    return cardValue[numberRandom];
-  }
+  return cardValue[numberRandom];
+}
 
-  console.log(generadedNumber());
-  console.log(generadedPalos());
+function selectColorSuit() {
+  let suitCards = document.querySelectorAll(".cardPalo");
+  suitCards.forEach(element => {
+    if (element.innerHTML === "♥" || element.innerHTML === "♦") {
+      element.style.color = "red";
+    } else {
+      element.style.color = "";
+    }
+  });
+}
 
+function generateCard() {
   let suitResult = generadedPalos();
-
   let suitCards = document.querySelectorAll(".cardPalo");
   suitCards.forEach(element => {
     element.innerHTML = suitResult;
   });
-  let selectColorSuit = fun;
 
   let numberResult = generadedNumber();
-
   let numberCards = document.querySelector(".card-title");
   numberCards.innerHTML = numberResult;
+
+  selectColorSuit();
+}
+
+window.onload = function() {
+  generateCard();
+
   let newCard = document.querySelector("#btn");
-  newCard.addEventListener("click", window.onload);
+  newCard.addEventListener("click", generateCard);
 };
